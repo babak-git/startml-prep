6fd05fc118005b686b7657cf77e8e20f9cdeb720
Andrii Babak <andreybabak1@icloud.com>
2025-11-28 18:09:03 +0000

    Update welcome message

--- hello_startml.py
+++ hello_startml.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello from PyCharm!")
-    print("Python + MedTech journey strats here.")
+    print("Road to AI/MedTech: Start ML → Start DL → CV.")
 
 if __name__ == "__main__":
     main()
